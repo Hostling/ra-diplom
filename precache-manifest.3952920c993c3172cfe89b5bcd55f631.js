44c763b2f8b9b372248917cfbbf4bbf3c2fa46a4
AppVeyor <ci@appveyor.com>
2020-05-13 05:38:17 +0000

    1c53aaf

---
+++ precache-manifest.3952920c993c3172cfe89b5bcd55f631.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec2aef0f1ac8b8bf7f7f93476e0ae65",
+    "url": "/ra-diplom/index.html"
+  },
+  {
+    "revision": "d3340c31cebe123236fc",
+    "url": "/ra-diplom/static/css/main.4ab2f43a.chunk.css"
+  },
+  {
+    "revision": "aec2c2b23cf1de9365f6",
+    "url": "/ra-diplom/static/js/2.c4090892.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ra-diplom/static/js/2.c4090892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3340c31cebe123236fc",
+    "url": "/ra-diplom/static/js/main.42c92474.chunk.js"
+  },
+  {
+    "revision": "ec6140ed330555cb3f92",
+    "url": "/ra-diplom/static/js/runtime-main.f864021e.js"
+  },
+  {
+    "revision": "d613d452bebe22d8b7d737ec12001e04",
+    "url": "/ra-diplom/static/media/banner.d613d452.jpg"
+  },
+  {
+    "revision": "9c087d4352a142067a9686012eb7ba57",
+    "url": "/ra-diplom/static/media/footer-sprite.9c087d43.png"
+  }
+]);
